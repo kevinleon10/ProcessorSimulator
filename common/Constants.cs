2829465dc8487a187697dda7823d483d0425e675
kevinleon10 <lskevin97@hotmail.com>
2018-06-10 08:43:47 -0500

    New common package

---
+++ common/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace ProcessorSimulator.common
+{
+    public static class Constants
+    {
+        public const int BytesInBlock = 16;
+        public const int WordsInBlock = 4;
+        public const int RegistersInCore = 32;
+        public const int InvalidLabel = -1;
+        public const int BlocksInMemory = 380;
+    }
+}
