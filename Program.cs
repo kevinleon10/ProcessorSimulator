71eb5b33d6ecc907b1417801367b002f7f473ebc
kevinleon10 <lskevin97@hotmail.com>
2018-06-14 14:20:57 -0500

    Deleted unnecesary method in main

--- Program.cs
+++ Program.cs
@@ -10,22 +10,8 @@ namespace ProcessorSimulator
 {
     internal class Program
     {
-        private static void Test(Context context)
-        {
-            Memory memory = null;
-            Cache<Instruction> instructionCache = new Cache<Instruction>(8, memory);
-            Cache<int> dataCache = new Cache<int>(8, memory);
-            var core = new Core(instructionCache, dataCache);
-            core.StartExecution(context);
-        }
-
         public static void Main(string[] args)
         {
-            /*
-            var context = new Context(200, 1);
-            var thread = new Thread(() => Test(context));
-            thread.Start();
-            */
             var processor = new Processor(10);
             Console.WriteLine("Hello world!");
         }
